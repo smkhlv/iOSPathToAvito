b1932b75b8bcd46c16126543fd4912a4b9867df6
Sergei <mikhaylovserj@gmail.com>
2024-04-11 13:29:11 +0500

    Initial Commit

---
+++ iOSPathToAvito/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iOSPathToAvito
+//
+//  Created by Sergei on 11.4.24..
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
